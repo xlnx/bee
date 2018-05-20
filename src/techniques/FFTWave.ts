11dd2ec8aadc923d8b277a3950f24dc526f8b0cf
xlnx <qwq@koishi.top>
2018-05-20 13:42:43 +0800

    FFT -> FFTWave

--- src/techniques/FFTWave.ts
+++ src/techniques/FFTWave.ts
@@ -1,19 +1,19 @@
 import { PostProcess } from "./postProcess";
 import { Shader } from "../gl/shader";
 import { Texture2D } from "../gl/texture";
-import { gl2, gl } from "../renderer/renderer";
+import { gl2, gl, Renderer } from "../renderer/renderer";
 import { Offscreen } from "../gl/offscreen";
 
-class FFT extends PostProcess {
+class FFTWave extends PostProcess {
 	private N: number;
 
-	constructor(private spectrum: Texture2D[]) {
+	constructor(private spectrum: Texture2D) {
 		super(Shader.create("fftsrc", false));
 		console.log(spectrum);
-		if (spectrum[0].width != spectrum[0].height) {
+		if (spectrum.width != spectrum.height) {
 			throw "FFT spectrum must be square.";
 		}
-		this.N = spectrum[0].width;
+		this.N = spectrum.width;
 		if (Math.pow(2, Math.log2(this.N)) != this.N) {
 			throw "FFT size must be 2^k";
 		}
@@ -34,14 +34,7 @@ class FFT extends PostProcess {
 			}, this.N, this.N);
 		}
 	}
-	private bindTextures() {
-		for (let i = 0; i != 3; ++i) {
-			this.offscreen.set(gl.COLOR_ATTACHMENT0 + i, this.textureB[i]);
-			gl.activeTexture(gl.TEXTURE0 + this.channel[i]);
-			gl.bindTexture(gl.TEXTURE_2D, this.textureA[i].handle);
-			this.gPrev[i].set(this.channel[i]);
-		}
-	}
+
 	render() {
 		this.offscreen.bind();
 			this.channel = [ 
@@ -50,56 +43,65 @@ class FFT extends PostProcess {
 				Texture2D.genChannel()
 			];
 			this.vao.bind();
+				// open MRT
 				gl2.drawBuffers([
 					gl.COLOR_ATTACHMENT0, 
 					gl2.COLOR_ATTACHMENT1,
 					gl2.COLOR_ATTACHMENT2
 				]);
-				this.vrs.use();
-					this.gN.set(this.N);
+				// generate channels: H(t), Dx(t), Dy(t)
+				this.fsrc.use();
 					for (let i = 0; i != 3; ++i) {
 						this.offscreen.set(gl.COLOR_ATTACHMENT0 + i, this.textureA[i]);
-						gl.activeTexture(gl.TEXTURE0 + this.channel[i]);
-						gl.bindTexture(gl.TEXTURE_2D, this.spectrum[i].handle);
-						this.gPrev[i].set(this.channel[i]);
 					}
+					gl.activeTexture(gl.TEXTURE0 + this.channel[0]);
+					gl.bindTexture(gl.TEXTURE_2D, this.spectrum.handle);
+					this.gSpectrum.set(this.channel[0]);
+					this.gTime.set(Renderer.time);
 					this.vao.draw();
+				this.fsrc.unuse();
+				// vertical bit-reversal copy
+				this.vrs.use();
+					this.gN.set(this.N);
+					this.bindTextures();
+					this.vao.draw();
+					this.swapTextures();
 				this.vrs.unuse();
+				// vertical FFT
 				this.vs.use();
 					this.gN.set(this.N);
 					for (let i = 1; i < this.N; i *= 2) {
 						this.gStep.set(i);
 						this.bindTextures();
 						this.vao.draw();
-						let t = this.textureA; this.textureA = this.textureB; this.textureB = t;
+						this.swapTextures();
 					}
 				this.vs.unuse();
+				// horizontal bit-reversal copy
 				this.hrs.use();
 					this.gN.set(this.N);
 					this.bindTextures();
 					this.vao.draw();
-					let t = this.textureA; this.textureA = this.textureB; this.textureB = t;
+					this.swapTextures();
 				this.hrs.unuse();
+				// horizontal FFT
 				this.hs.use();
 					this.gN.set(this.N);
 					for (let i = 1; i < this.N; i *= 2) {
 						this.gStep.set(i);
 						this.bindTextures();
 						this.vao.draw();
-						let t = this.textureA; this.textureA = this.textureB; this.textureB = t;
+						this.swapTextures();
 					}
 				this.hs.unuse();
+				// combine H, Dx, Dy into displacement map
 				this.fend.use();
 					this.gN.set(this.N);
-					// for (let i = 0; i != 3; ++i) {
-					// 	gl.activeTexture(gl.TEXTURE0 + this.channel[i]);
-					// 	gl.bindTexture(gl.TEXTURE_2D, this.textureA[i].handle);
-					// 	this.gPrev[i].set(this.channel[i]);
-					// }
 					this.bindTextures();
 					this.offscreen.set(gl.COLOR_ATTACHMENT0, this.displacement);
 					this.vao.draw();
 				this.fend.unuse();
+				// close MRT
 				gl2.drawBuffers([
 					gl.COLOR_ATTACHMENT0
 				]);
@@ -114,8 +116,17 @@ class FFT extends PostProcess {
 		return this.displacement;
 	}
 
-	get textures(): Texture2D[] {
-		return this.textureA;
+	private bindTextures() {
+		for (let i = 0; i != 3; ++i) {
+			this.offscreen.set(gl.COLOR_ATTACHMENT0 + i, this.textureB[i]);
+			gl.activeTexture(gl.TEXTURE0 + this.channel[i]);
+			gl.bindTexture(gl.TEXTURE_2D, this.textureA[i].handle);
+			this.gPrev[i].set(this.channel[i]);
+		}
+	}
+
+	private swapTextures() {
+		let t = this.textureA; this.textureA = this.textureB; this.textureB = t;
 	}
 
 	private textureA: Texture2D[] = [];
@@ -127,6 +138,7 @@ class FFT extends PostProcess {
 
 	private offscreen = new Offscreen();
 
+	private gTime = Shader.uniform("float", "gTime");
 	private gN = Shader.uniform("float", "gN");
 	private gStep = Shader.uniform("float", "gStep");
 	private gPrev = [ 
@@ -134,7 +146,9 @@ class FFT extends PostProcess {
 		Shader.uniform("int", "gPrevDx"),
 		Shader.uniform("int", "gPrevDy")
 	];
+	private gSpectrum = Shader.uniform("int", "gSpectrum");
 
+	private fsrc = Shader.create("fftsrc", false);
 	private vs = Shader.create("fftv", false);
 	private hs = Shader.create("ffth", false);
 	private vrs = Shader.create("fftvr", false);
@@ -143,5 +157,5 @@ class FFT extends PostProcess {
 }
 
 export {
-	FFT
+	FFTWave
 }
